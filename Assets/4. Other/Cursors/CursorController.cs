c14aaa927cb1d5ccb705c22d1f8c89eb22659f3b
TruongHoang <sinlotaradin@gmail.com>
2020-06-30 16:27:42 +0700

    add start menu

--- Assets/4. Other/Cursors/CursorController.cs
+++ Assets/4. Other/Cursors/CursorController.cs
@@ -14,6 +14,10 @@ public class CursorController : MonoBehaviour
 
     private void Update()
     {
+        if (Camera.main == null)
+        {
+            return;
+        }
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
